cc5ef6a695feabe28e9fd3a9282be62a351a8f5e
Jon Dugan <jdugan@x1024.net>
2018-03-03 11:08:30 -0600

    Remove details from nominees; update image

--- data-loading/convert_data_2018.py
+++ data-loading/convert_data_2018.py
@@ -3,6 +3,21 @@ import json
 # from http://d37lefl1k5vay2.cloudfront.net/api/1.0/pages/nominees%2F2018/
 data = json.loads(open("./2018oscars_raw.json").read())
 categories = json.loads(open("category-points.json").read())
+remove_details = [
+    "Best Picture",
+    "Animated Feature Film",
+    "Documentary Feature",
+    "Costume Design",
+    "Production Design",
+    "Visual Effects",
+    "Makeup And Hairstyling",
+    "Short Film Live Action",
+    "Documentary Short",
+    "Short Film Animated",
+    "Film Editing",
+    "Sound Editing",
+    "Sound Mixing",
+]
 
 def reformat(section):
     return " ".join([x.capitalize() for x in section.split("-")])
@@ -28,9 +43,15 @@ for cat, v in data["data"]["sections"]["nominees"].items():
     points = categories[category]
     order = v["order"]
     cat_obj = Category(category, points, order, [])
-    print category, points, order
+    print category, points, order, category in remove_details
     for nominee in v["result"]:
-        nom = u"{} ({})".format(nominee["post_title"], nominee["nominee_description"])
+        if category in remove_details:
+            nom = nominee["post_title"]
+        elif category == "Foreign Language Film":
+            country = nominee["nominee_description"].split(";")[0]
+            nom = u"{} ({})".format(nominee["post_title"], country)
+        else:
+            nom = u"{} ({})".format(nominee["post_title"], nominee["nominee_description"])
         cat_obj.nominees.append(nom)
     cat_list.append(cat_obj)
 
--- data-loading/insert_data.py
+++ data-loading/insert_data.py
@@ -5,18 +5,22 @@ cursor = dbh.cursor()
 
 data = json.loads(open("2018.json").read())
 
-for c in data:
-    name = c["name"]
-    points = c["points"]
-    order = c["order"]
-    print u"{} {} {}".format(name, points, order)
-    q = "INSERT into oscars.category (name, points, display_order) values (%s, %s, %s) returning id"
-    cursor.execute(q, (name, points, order))
-    cat_id = cursor.fetchone()[0]
-    print "catid", cat_id
-    for n in c["nominees"]:
-        print u"    {}".format(n)
-        qnom = "INSERT into oscars.nominee (name, category_id) values (%s, %s)"
-        cursor.execute(qnom, (n, cat_id))
 
-dbh.commit()
+def insert_data():
+    for c in data:
+        name = c["name"]
+        points = c["points"]
+        order = c["order"]
+        print u"{} {} {}".format(name, points, order)
+        q = "INSERT into oscars.category (name, points, display_order) values (%s, %s, %s) returning id"
+        cursor.execute(q, (name, points, order))
+        cat_id = cursor.fetchone()[0]
+        print "catid", cat_id
+        for n in c["nominees"]:
+            print u"    {}".format(n)
+            qnom = "INSERT into oscars.nominee (name, category_id) values (%s, %s)"
+            cursor.execute(qnom, (n, cat_id))
+
+    dbh.commit()
+
+insert_data()
